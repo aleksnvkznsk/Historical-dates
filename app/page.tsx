792e13cf558b3387aef9bf2529c98bfbe0344d3d
aleksnvkznsk <aleksandrov.developer@gmail.com>
2023-08-24 10:28:38 +0700

    create hoop and events

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { dates } from '@/_data/dates';
 import Container from '@/components/Container/Container';
 
 export default function Home() {
-  return <Container/>
+  return <Container dates={dates}/>
 }
